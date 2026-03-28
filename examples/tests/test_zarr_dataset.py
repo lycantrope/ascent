2792b4d6d0672a3ab60f16b54b947bf79dad4e71
Chung-Kuan Chen <b97b01045@gmail.com>
2026-03-28 15:00:27 +0900

    Update examples/tests/test_zarr_dataset.py

--- examples/tests/test_zarr_dataset.py
+++ examples/tests/test_zarr_dataset.py
@@ -1,3 +1,4 @@
+import time
 from pathlib import Path
 
 import numpy as np
@@ -5,7 +6,7 @@ from torch.utils.data import DataLoader
 
 from ascent.datasets.tracking_dataset import ObjectEmbeddingDataset3D
 
-home = Path(r"D:\kuan\zeng-nwb\TOY21\251203-1DA\activity\W4\hdf-sequence")
+home = Path(".")
 
 
 hdf_path = home / "crop-W4-activity.h5"
@@ -13,28 +14,80 @@ zarr_path = home / "crop-W4-activity.zarr.zip"
 
 coord_files = home / "crop-W4-activity_peaks.csv"
 
-hdf_dset = ObjectEmbeddingDataset3D(
-    image_file=str(hdf_path),
-    coord_file=str(coord_files),
-)
 
-zarr_dset = ObjectEmbeddingDataset3D(
-    image_file=str(zarr_path),
-    coord_file=str(coord_files),
-)
+def verification():
+
+    hdf_dset = ObjectEmbeddingDataset3D(
+        image_file=str(hdf_path),
+        coord_file=str(coord_files),
+    )
+    zarr_dset = ObjectEmbeddingDataset3D(
+        image_file=str(zarr_path),
+        coord_file=str(coord_files),
+    )
+
+    hdf_loader = DataLoader(hdf_dset, batch_size=1, shuffle=False, drop_last=False)
+    zarr_loader = DataLoader(zarr_dset, batch_size=1, shuffle=False, drop_last=False)
+
+    for i, (d1, d2) in enumerate(zip(hdf_loader, zarr_loader)):
+        for (k1, v1), (k2, v2) in zip(d1.items(), d2.items()):
+            try:
+                np.testing.assert_equal(np.array(v1), np.array(v2))
+
+            except AssertionError as e:
+                print(f"{k1} is unequal")
+                raise e
+        if i > 10:
+            print("No problem")
+            break
 
 
-hdf_loader = DataLoader(hdf_dset, batch_size=1, shuffle=False, drop_last=False)
-zarr_loader = DataLoader(zarr_dset, batch_size=1, shuffle=False, drop_last=False)
+if __name__ == "__main__":
+    verification()
+    # Benchmark
+    for num_workers in [1, 2, 4]:
+        for batch_size in [1, 2, 4]:
+            print(f"Zarr|n_workers:{num_workers:d}|batch_size:{batch_size:d}", end="")
 
-for i, (d1, d2) in enumerate(zip(hdf_loader, zarr_loader)):
-    for (k1, v1), (k2, v2) in zip(d1.items(), d2.items()):
-        try:
-            np.testing.assert_equal(np.array(v1), v2)
+            zarr_dset = ObjectEmbeddingDataset3D(
+                image_file=str(zarr_path),
+                coord_file=str(coord_files),
+            )
+            loader = DataLoader(
+                zarr_dset,
+                batch_size=batch_size,
+                num_workers=num_workers,
+                shuffle=False,
+                drop_last=False,
+            )
+            t0 = time.perf_counter()
+            for i, d in enumerate(loader):
+                if i > 20:
+                    break
 
-        except AssertionError as e:
-            print(f"{k1} is unequal")
-            raise e
-    if i > 10:
-        print("No problem")
-        break
+            print(f"|elapsed time:{time.perf_counter()-t0:.3f}s")
+
+    for num_workers in [1, 2, 4]:
+        for batch_size in [1, 2, 4]:
+
+            print(f"HDF5|n_workers:{num_workers:d}|batch_size:{batch_size:d}", end="")
+
+            hdf_dset = ObjectEmbeddingDataset3D(
+                image_file=str(hdf_path),
+                coord_file=str(coord_files),
+                lazy_loading=True,
+            )
+
+            loader = DataLoader(
+                hdf_dset,
+                batch_size=batch_size,
+                num_workers=num_workers,
+                shuffle=False,
+                drop_last=False,
+                prefetch_factor=2,
+            )
+            t0 = time.perf_counter()
+            for i, d in enumerate(loader):
+                if i > 20:
+                    break
+            print(f"|elapsed time:{time.perf_counter()-t0:.3f}s")
